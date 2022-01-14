b4985d3408fc0b809cfcea4f8e3a933dcc6a10b6
vitalij-lutsyk <lutsyk.vitalij@gmail.com>
2022-01-15 01:21:05 +0200

    feat: add base interfaces

---
+++ src/interfaces/api-response.ts
@@ -0,0 +1,48 @@
+export interface OverpassApiRes {
+  elements: Array<OverpassApiResItem>;
+  generator: string; // Overpass API {version} {id}
+  osm3s: Osm3s;
+  version: number;
+}
+
+interface Osm3s {
+  copyright: string;
+  timestamp_osm_base: string;
+}
+
+interface OverpassApiResItem {
+  bounds: Bounds;
+  geometry: Array<Geometry>;
+  id: number;
+  nodes?: Array<number>;
+  members?: Array<RelationMember>
+  tags: Tags;
+  type: 'way' | 'relation'; // TODO: enum
+}
+
+interface RelationMember {
+  geometry: Array<Geometry>;
+  ref: number;
+  role: 'outer' | 'inner' | 'outline'; // TODO: enum
+  type: 'way';
+}
+
+interface Tags {
+  ["addr:housenumber"]: string,
+  ["addr:street"]: string,
+  name: string,
+  start_date: string,
+  wikipedia: string,
+}
+
+interface Geometry {
+  lat: number;
+  lon: number;
+}
+
+interface Bounds {
+  maxlat: number;
+  maxlon: number;
+  minlat: number;
+  minlon: number;
+}
